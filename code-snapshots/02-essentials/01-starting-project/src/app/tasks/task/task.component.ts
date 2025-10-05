74aee3b1c79a9b3b96b63d77577f8d46afa5dd4a
Bijan Camp <8800015+bijancamp@users.noreply.github.com>
2025-10-05 20:46:07 +0000

    Time to Practice: Services

--- code-snapshots/02-essentials/01-starting-project/src/app/tasks/task/task.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import { CardComponent } from "../../shared/card/card.component";
 import { type Task } from './task.model';
+import { TasksService } from '../tasks.service';
+import { CardComponent } from "../../shared/card/card.component";
 
 @Component({
   selector: 'app-task',
@@ -12,9 +13,10 @@ import { type Task } from './task.model';
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+
+  constructor(private tasksService: TasksService) {}
 
   onCompleteTask() {
-    this.complete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id);
   }
 }

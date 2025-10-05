5bee60f64d27ef2b9a66c05c1da8abd91366372a
Bijan Camp <8800015+bijancamp@users.noreply.github.com>
2025-10-05 12:47:31 -0500

    Getting Started with Dependency Injection

--- code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { TasksService } from "./tasks.service";
 import { type NewTask } from './new-task/new-task.model';
 import { NewTaskComponent } from "./new-task/new-task.component";
 import { TaskComponent } from "./task/task.component";
@@ -14,36 +15,15 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   isAddingTask = false;
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary: 'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ]
+
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasks.filter(task => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    this.tasksService.removeTask(id);
   }
 
   onStartAddTask() {
@@ -57,10 +37,6 @@ export class TasksComponent {
   onAddTask(newTask: NewTask) {
     this.isAddingTask = false;
 
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      ...newTask
-    });
+    this.tasksService.addTask(newTask, this.userId);
   }
 }

24bfe5cbb38364b634e115246875ea77f2d277ec
Bijan Camp <8800015+bijancamp@users.noreply.github.com>
2025-11-01 22:02:17 +0000

    Reset form

--- code-snapshots/06-cmp-deep-dive/01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ code-snapshots/06-cmp-deep-dive/01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -11,8 +11,9 @@ import { ControlComponent } from "../../../shared/control/control.component";
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  onSubmit(title: string, ticketText: string) {
+  onSubmit(title: string, ticketText: string, form: HTMLFormElement) {
     console.log(title);
     console.log(ticketText);
+    form.reset();
   }
 }

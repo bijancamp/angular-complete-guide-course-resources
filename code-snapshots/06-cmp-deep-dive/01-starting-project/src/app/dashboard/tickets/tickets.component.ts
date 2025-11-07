027ecf44bc8d92df0a1fb5a8d62f2040e59a5723
Bijan Camp <8800015+bijancamp@users.noreply.github.com>
2025-11-07 21:24:57 +0000

    Cross-Component Communication & State Management

--- code-snapshots/06-cmp-deep-dive/01-starting-project/src/app/dashboard/tickets/tickets.component.ts
+++ code-snapshots/06-cmp-deep-dive/01-starting-project/src/app/dashboard/tickets/tickets.component.ts
@@ -21,4 +21,14 @@ export class TicketsComponent {
       status: 'open'
     });
   }
+
+  onCloseTicket(id: string) {
+    this.tickets = this.tickets.map(ticket => {
+      if (ticket.id === id) {
+        return { ...ticket, status: 'closed' };
+      }
+
+      return ticket;
+    })
+  }
 }

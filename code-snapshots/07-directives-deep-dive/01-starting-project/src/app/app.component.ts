6206cdedf1a054206fb88e0a85c6628625a463fd
Bijan Camp <8800015+bijancamp@users.noreply.github.com>
2025-11-21 21:37:40 +0000

    Host Directives & Composition

--- code-snapshots/07-directives-deep-dive/01-starting-project/src/app/app.component.ts
+++ code-snapshots/07-directives-deep-dive/01-starting-project/src/app/app.component.ts
@@ -5,13 +5,14 @@ import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { AuthService } from './auth/auth.service';
 import { AuthDirective } from './auth/auth.directive';
+import { LogDirective } from './log.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, AuthDirective, LearningResourcesComponent, NgIf],
+  imports: [AuthComponent, AuthDirective, LearningResourcesComponent, LogDirective, NgIf],
 })
 export class AppComponent {
   private authService = inject(AuthService);
